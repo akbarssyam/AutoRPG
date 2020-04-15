260232d7a3040466ee75d092f1efbd23006d518f
akbarssyam <akbar.syam@isid.co.jp>
2020-04-16 00:20:29 +0900

    Added images, floating damage and accuracy system

---
+++ Assets/DamagePopup.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DamagePopup : MonoBehaviour
+{
+    TextMeshPro tmp;
+    Color textColor;
+    float lifetime = 0.5f;
+
+    public static DamagePopup Create(Vector3 pos, int damageAmount)
+    {
+        Transform damagePopupTransform = Instantiate(GameAssets.Instance.damagePopup, pos, Quaternion.identity);
+
+        DamagePopup damagePopup = damagePopupTransform.GetComponent<DamagePopup>();
+        damagePopup.Setup(damageAmount);
+
+        return damagePopup;
+    }
+
+    public static DamagePopup Create(Vector3 pos, string text)
+    {
+        Transform damagePopupTransform = Instantiate(GameAssets.Instance.damagePopup, pos, Quaternion.identity);
+
+        DamagePopup damagePopup = damagePopupTransform.GetComponent<DamagePopup>();
+        damagePopup.Setup(text);
+
+        return damagePopup;
+    }
+
+    void Awake()
+    {
+        tmp = GetComponent<TextMeshPro>();
+    }
+
+    public void Setup(int damageAmount)
+    {
+        tmp.SetText(damageAmount.ToString());
+        textColor = tmp.color;
+    }
+
+    public void Setup(string text)
+    {
+        tmp.SetText(text);
+        textColor = tmp.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveSpeed = -1f;
+        transform.position += new Vector3(0, moveSpeed) * Time.deltaTime;
+
+        lifetime -= Time.deltaTime;
+        if (lifetime <= 0)
+        {
+            // Start disappearing
+            float disappearSpeed = 3f;
+            textColor.a -= disappearSpeed * Time.deltaTime;
+            tmp.color = textColor;
+            if (textColor.a <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField]
+    private Image foregroundImage;
+    [SerializeField]
+    private float updateSpeedSeconds = 0.1f;
+
+    private void Start()
+    {
+        GetComponentInParent<Hero>().OnHealthPctChanged += HandleHealthChanged;
+    }
+
+    private void HandleHealthChanged(float pct)
+    {
+        StartCoroutine(ChangeToPct(pct));
+    }
+
+    IEnumerator ChangeToPct(float pct)
+    {
+        float before = foregroundImage.fillAmount;
+        float elapsed = 0f;
+
+        while (elapsed < updateSpeedSeconds)
+        {
+            elapsed += Time.deltaTime;
+            foregroundImage.fillAmount = Mathf.Lerp(before, pct, elapsed / updateSpeedSeconds);
+            yield return null;
+        }
+
+        foregroundImage.fillAmount = pct;
+    }
+ 
+}
